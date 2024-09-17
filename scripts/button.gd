44ba19d746bb4e4c4733a62cdb68bb7d73d8f033
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2024-09-17 21:25:35 +0200

    lanterns cursor fixed

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,10 +1,25 @@
 extends Button
 
 signal switch
+var hand_cursor = preload("res://assets/cursors/hand_cursor.png")
+var normal_cursor = preload("res://assets/cursors/normal_cursor.png")
+
 
 func _ready():
 	connect("pressed", light_change)
 	
+	connect("mouse_entered", change_cursor)
+	connect("mouse_exited", reset_cursor)
+	
 func light_change():
 	#print("click")
-	emit_signal("switch", self.get_parent().get_parent().name)
+	if self.name == "CandleButton":
+		emit_signal("switch", self.get_parent().get_parent().name)
+	else:
+		emit_signal("switch", self.get_parent().name)
+
+func change_cursor():
+	Input.set_custom_mouse_cursor(hand_cursor, Input.CURSOR_ARROW, Vector2(6, 7))
+
+func reset_cursor():
+	Input.set_custom_mouse_cursor(normal_cursor, Input.CURSOR_ARROW, Vector2(5, 7))
