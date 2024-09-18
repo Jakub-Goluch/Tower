2f653e02a501e3c587cf1ecb1ad087d8a057cecb
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2024-09-18 22:10:24 +0200

    book fixed, font added

--- scripts/book_stand.gd
+++ scripts/book_stand.gd
@@ -14,11 +14,12 @@ func _ready():
 	$StandButton.connect("mouse_entered", change_cursor)
 	$StandButton.connect("mouse_exited", reset_cursor)
 	book_open = false
-	$BookCanvasLayer.visible = false
+	$BookCanvasLayer.hide()
 	
 	# to close big book ----------------
 	$BookCanvasLayer/Book/AnimatedSprite2D/Sprite2D2/CloseBookButton.connect("hide", close)
 	# -----------------------------------
+
 
 	
 func _process(delta):
@@ -26,11 +27,18 @@ func _process(delta):
 		Input.set_custom_mouse_cursor(normal_cursor, Input.CURSOR_ARROW, Vector2(5, 7))
 	
 func open():
-	$BookCanvasLayer.visible = true
+	$BookCanvasLayer.show()
 	book_open = true
+	$BookCanvasLayer/Book/AnimatedSprite2D/FlipPage/FlipButton.mouse_filter = 0
+	$BookCanvasLayer/Book/AnimatedSprite2D/FlipPageBack/FlipButtonBack.mouse_filter = 0
+	$BookCanvasLayer/Book/AnimatedSprite2D/Sprite2D2/CloseBookButton.mouse_filter = 0
 
 func close():
-	$BookCanvasLayer.visible = false
+	$BookCanvasLayer.hide()
+	$BookCanvasLayer/Book/AnimatedSprite2D/FlipPage/FlipButton.mouse_filter = 1
+	$BookCanvasLayer/Book/AnimatedSprite2D/FlipPageBack/FlipButtonBack.mouse_filter = 1
+	$BookCanvasLayer/Book/AnimatedSprite2D/Sprite2D2/CloseBookButton.mouse_filter = 1
+	book_open = false
 	
 func change_cursor():
 	if !book_open:
