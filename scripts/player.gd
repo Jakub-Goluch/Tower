54096d4ff029e244256478ca5f8f991f99cfa3fd
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2024-09-19 13:50:22 +0200

    door teleportation and animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
-var speed = 100.0
-var click_position = Vector2()
-var target_position = Vector2()
 
-# so the player doesn't move back to 0,0 on start
-func _ready():
-	click_position = position
+const SPEED = 80.0
+const JUMP_VELOCITY = 0.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("left_mouse"):
-		click_position  = get_global_mouse_position()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction: # right
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_pressed("ui_left"):
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
+	elif Input.is_action_pressed("ui_right"):
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
+	elif Input.is_action_just_released("ui_left") || Input.is_action_just_released("ui_right"):
+		animated_sprite.flip_h = false
+		animated_sprite.play("idle")
 	
-	if position.distance_to(target_position) < 3:
-		$AnimatedSprite2D.play("idle")
-		
-	if position.distance_to(target_position) > 3:
-		target_position = (click_position - position).normalized()
-		velocity = target_position * speed
-		
-		if click_position.x > position.x:
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.play("walk")
-		elif click_position.x < position.x:
-			$AnimatedSprite2D.flip_h = false
-			$AnimatedSprite2D.play("walk")
-		move_and_slide()
+	move_and_slide()
