6a37fd20ad956967e30a3565a42e360d646e465e
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2024-09-22 12:56:20 +0200

    bug fixes

--- scripts/camera_change_area.gd
+++ scripts/camera_change_area.gd
@@ -4,21 +4,22 @@ var counter = 0
 @onready var camera: Camera2D = %Camera2D
 @onready var player: CharacterBody2D = %Player
 var right_position: Vector2
-var left_position: Vector2
-var old_position: Vector2
+#var left_position: Vector2
+#var old_position: Vector2
 
 func _ready():
 	connect("body_entered", move_camera)
-	right_position = Vector2(386.0 , 220.0) # elixir office
-	left_position = Vector2(147.0, 196.0) # main bedroom
+	right_position = Vector2(382.0 , 215.0) # elixir office
+	#left_position = Vector2(154.0, 196.0) # main bedroom
 	
 func move_camera(body):
 	print("player entered area!")
-	counter += 1
-	old_position = camera.global_position
+	#counter += 1
+	#old_position = camera.global_position
+	print("moving RIGHT")
+	camera.position = right_position
 	
-	if(counter%2 != 0): # entering from left so move to right
-		camera.position = right_position
-	elif(counter%2==0): # entering from right so move to left
-		camera.position = left_position
+	#elif name == "CameraChangeAreaLeft": # move to left room
+		#print("moving LEFT")
+		#camera.global_position = left_position
 	
--- scripts/camera_change_main_hall_left.gd
+++ scripts/camera_change_main_hall_left.gd
@@ -3,20 +3,19 @@ extends Area2D
 var counter = 0
 @onready var camera: Camera2D = %Camera2D
 @onready var player: CharacterBody2D = %Player
-var right_position: Vector2
-#var left_position: Vector2
+#var right_position: Vector2
+var left_position: Vector2
 #var old_position: Vector2
 
 func _ready():
 	connect("body_entered", move_camera)
-	right_position = Vector2(43.0 , 599.0) # elixir office
-	#left_position = Vector2(-179.0, 549.0) # main bedroom
+	#right_position = Vector2(108.0 , 464.0) # elixir office
+	left_position = Vector2(-169.0, 423.0) # main bedroom
 	
 func move_camera(body):
-	print("player entered area!")
+	#print("player entered area!")
 	#counter += 1
 	#old_position = camera.global_position
 	
-	print("moving RIGHT")
-	camera.position = right_position
-	
+	print("moving LEFT")
+	camera.position = left_position
